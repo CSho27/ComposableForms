bcc9959f34cc9b135b9d739d4344bf8d2a11c2af
Chris Shorter <shorte24@gmail.com>
2024-12-22 16:51:38 -0500

    Implements first pass at composable reducer

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -1,16 +1,20 @@
 import { describe, it, expect } from "@jest/globals";
-import { Reducer } from "./reducer";
+import { ComposableAction, ComposableStateReducer } from "./reducer";
 
 describe("Reducer", () => {
   const initialState: TestState = { name: null, quantity: 1 };
 
   describe("update", () => {
     it("Should update a property", () => {
-      const result = Reducer(initialState, {
-        type: "update",
-        property: "name",
-        value: "Chris",
-      });
+      const result = ComposableStateReducer(
+        initialState,
+        {
+          type: "update",
+          property: "name",
+          value: "Chris",
+        },
+        TestStateReducer
+      );
       expect(result.name).toBe("Chris");
     });
   });
@@ -19,4 +23,20 @@ describe("Reducer", () => {
 export interface TestState {
   name: string | null;
   quantity: number;
+}
+
+export type TestStateAction = { type: "reset" };
+
+export function TestStateReducer(
+  state: TestState,
+  action: ComposableAction<TestState, TestStateAction>
+): TestState | undefined {
+  switch (action.type) {
+    case "reset":
+      return {
+        ...state,
+        name: null,
+        quantity: 1,
+      };
+  }
 }
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,28 +1,59 @@
 export type State = Record<string, any>;
 export type Path<TState> = Extract<keyof TState, string>;
-
-export interface Action {
+export type Action = {
   type: string;
+};
+
+export type PropertyAction<TState> = {
+  [P in Path<TState>]: {
+    type: "update";
+    property: P;
+    value: TState[P];
+  };
+}[Path<TState>];
+
+export function isPropertyAction<TState extends State>(
+  state: TState,
+  action: Action
+): action is PropertyAction<TState> {
+  if (action.type !== "update") return false;
+  const propertyAction = action as PropertyAction<TState>;
+  const isValidProperty = Object.keys(state).some(
+    (k) => k === propertyAction.property
+  );
+  return isValidProperty;
 }
 
-export type PropertyAction<TState> = Action &
-  {
-    [P in Path<TState>]: {
-      type: "update";
-      property: P;
-      value: TState[P];
-    };
-  }[Path<TState>];
-
-export function Reducer<TState extends State>(
+export type ComposableAction<TState extends State, TAction extends Action> =
+  | TAction
+  | PropertyAction<TState>;
+export type ComposableReducer<TState extends State, TAction extends Action> = (
   state: TState,
-  action: PropertyAction<TState>
-) {
+  action: ComposableAction<TState, TAction>
+) => TState | undefined;
+
+export function ComposableStateReducer<
+  TState extends State,
+  TAction extends Action
+>(
+  state: TState,
+  action: ComposableAction<TState, TAction>,
+  reducer?: ComposableReducer<TState, TAction>
+): TState {
+  const updatedState = reducer?.(state, action);
+  if (updatedState !== undefined) return updatedState;
+
   switch (action.type) {
     case "update":
+      if (!isPropertyAction(state, action))
+        throw new Error("Invalid update action.");
+
       return {
         ...state,
         [action.property]: action.value,
       };
+    default:
+      console.warn("Action fell through. No operation will be performed.");
+      return state;
   }
 }
