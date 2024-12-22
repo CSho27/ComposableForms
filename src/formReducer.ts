9a4d5140145bff583f6296e6c91f1346fc7729c0
Chris Shorter <shorte24@gmail.com>
2024-12-22 12:55:23 -0500

    Sets up the beginnings of the formReducer

---
+++ src/formReducer.ts
@@ -0,0 +1,33 @@
+export type State = Record<string, any>;
+export type Path<TState> = Extract<keyof TState, string>
+export type PathValue<TState, TPath extends Path<TState>> = TState[TPath];
+
+export interface FormState<TState extends State> {
+  state: TState;
+}
+
+export type FormAction<TState> = PropertyAction<TState>;
+
+export interface Action { 
+  type: string; 
+}
+
+export type PropertyAction<TState> = Action & {
+  [K in Path<TState>]: { type: 'update', property: K, value: TState[K] };
+}[Path<TState>];
+
+export function FormEditorStateReducer<TState extends State, TAction extends FormAction<TState>>(state: TState, action: TAction): TState {
+  switch(action.type) {
+    case 'update':
+      return {
+        ...state,
+        [action.property]: action.value
+      }
+  }
+}
+
+export interface TestState {
+  name: string;
+  quantity: number;
+  addedOn: Date;
+}
